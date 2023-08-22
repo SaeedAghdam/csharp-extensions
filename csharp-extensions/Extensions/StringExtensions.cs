26aa75c2701bdcc03740de080993879b8d34163d
Saeed Aghdam <saeed.snfa@hotmail.com>
2023-08-21 22:52:50 -0400

    Added convert case extension method

--- csharp-extensions/Extensions/StringExtensions.cs
+++ csharp-extensions/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace csharp_extensions.Extensions
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace csharp_extensions.Extensions
 {
     public static class StringExtensions
     {
@@ -37,6 +40,45 @@
 
             return newStr.Split(value).Length - 1;
 
+        }
+
+        public static string ConvertCase(this string str, StringConvertCaseType stringConvertCaseType)
+        {
+            if (str == null)
+                throw new ArgumentNullException(nameof(str));
+
+            switch (stringConvertCaseType)
+            {
+                case StringConvertCaseType.TitleCase:
+                    return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+                case StringConvertCaseType.LowerCase:
+                    return str.ToLower();
+                case StringConvertCaseType.UpperCase:
+                    return str.ToUpper();
+                case StringConvertCaseType.SentenceCase:
+                    var regex = new Regex(@"(^[a-z])|\.\s+(.)", RegexOptions.ExplicitCapture);
+                    return regex.Replace(str.ToLower(), s => s.Value.ToUpper());
+                case StringConvertCaseType.InvertCase:
+                    return new string(str.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ?
+                       char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+                case StringConvertCaseType.RandomCase:
+                    var random = new Random();
+                    return new string(str.Select(c => char.IsLetter(c) ? (random.Next(2) == 1 ?
+                      char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+                default:
+                    return str;
+            }
+        }
+
+
+        public enum StringConvertCaseType
+        {
+            TitleCase,
+            LowerCase,
+            UpperCase,
+            SentenceCase,
+            InvertCase,
+            RandomCase
         }
     }
 }
